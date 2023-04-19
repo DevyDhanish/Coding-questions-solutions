34abc758c6b1b49eed82de21c572ba6a2de3646f
DevyDhanish <dhanishkariyatmain@gmail.com>
2023-04-19 22:30:32 +0530

    Modified atoi

--- atoi.cpp
+++ atoi.cpp
@@ -1,62 +1,47 @@
 #include <iostream>
 #include <map>
+#include <climits>
 
 using std::cout;
 using std::map;
 using std::string;
 
-class Solution
-{
+class Solution {
 public:
-    int myAtoi(string s)
-    {
-        int sum = 0;
-        int p1 = 0;
-        int p2 = p1 + 1;
-        int sign = 1;
-        map<char, int> numbers = {
-            {'0', 0},
-            {'1', 1},
-            {'2', 2},
-            {'3', 3},
-            {'4', 4},
-            {'5', 5},
-            {'6', 6},
-            {'7', 7},
-            {'8', 8},
-            {'9', 9}};
-        
-        while(true){
-            if(s[p1] == '-') sign = -1;
-
-            if(p2 > s.length()){
-                sum = sum * 1 + numbers[s[p1]];
-                break;
-            }
-
-            if(s[p1] != ' ' && s[p2] == ' '){ 
-                sum = sum * 10 + numbers[s[p1]];
-                break;
-            }
-
-            if(sum >= 2147483647 && sum <= -2147483647){
-                sum = 2147483647;
-                break;
-            }
-
-            if(s[p1] != ' ') sum = sum * 10 + numbers[s[p1]];
-
-            p1++;
-            p2++;
-        }
-
-        return sum * sign;
+    int myAtoi(string s) {
+       string str="";
+       int minus = 1;
+       int flag = 0;
+       int i = 0;
+       while((s[i] == ' '))i++; //removing all spaces
+       if(s[i] == '-' || s[i] == '+'){
+          if(s[i] == '-')minus = -1;
+           i++;
+       }//seeing weather the number is pos or neg
+    
+       for(;i<s.length();i++){
+          if(s[i] >= 48 && (s[i] <= 57)){
+            str = str + s[i];
+          }
+          else break;
+       }
+       if(str.length() == 0)return 0;
+       int res = 0;
+       for(int i=0;i<str.length();i++){
+           long long res_temp = res;
+           if(res_temp*10+int(str[i] - 48) > INT_MAX){
+               if(minus == -1)return 2147483648*minus;//for neg elements it is 2^31
+               else return 2147483647;//for positive elements it is 2^31-1
+           }
+           res  = res*10 + int(str[i] - 48);
+       }//converting to string
+       return res*minus;
     }
 };
 
 int main()
 {
     Solution sol;
-    auto a = sol.myAtoi("-91283472332");
+    auto a = sol.myAtoi("3.14159");
     cout << a;
 }
